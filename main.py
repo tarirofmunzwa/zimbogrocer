04e52e383516f8a12a115aa8d2aad65ac5a1e01d
ATHUL A S <64469847+AthulNoobie@users.noreply.github.com>
2024-06-23 20:42:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -122,6 +122,6 @@ def webhook():
             else:send(reply,sender,phone_id)
         except:pass
         return jsonify({"status": "ok"}), 200
-    else:return "WhatsApp Bot is Running"
+    return "WhatsApp Bot is Running"
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
