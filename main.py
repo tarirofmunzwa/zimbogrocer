7171262c8b5291de04268fc46acbed4a91612bf1
ATHUL A S <64469847+AthulNoobie@users.noreply.github.com>
2024-06-28 19:31:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ from mimetypes import guess_type
 import psycopg2
 from datetime import datetime,timedelta
 from urlextract import URLExtract
-from urlextract.cachefile import CacheFile
 from training import instructions
 import sched
 import time
 
 
-db=True
+db=False
 wa_token=os.environ.get("WA_TOKEN") # Whatsapp API Key
 gen_api=os.environ.get("GEN_API") # Gemini API Key
 owner_phone=os.environ.get("OWNER_PHONE") # Owner's phone number with countrycod
 model_name="gemini-1.5-flash-latest"
 
-app = Flask(__name__)
+app=Flask(__name__)
 genai.configure(api_key=gen_api)
+
 class CustomURLExtract(URLExtract):
     def _get_cache_file_path(self):
         cache_dir = "/tmp"
         return os.path.join(cache_dir, "tlds-alpha-by-domain.txt")
 
 extractor = CustomURLExtract(limit=1)
+
 generation_config = {
   "temperature": 1,
   "top_p": 0.95,
@@ -47,19 +48,41 @@ model = genai.GenerativeModel(model_name=model_name,
 
 convo = model.start_chat(history=[])
 
+convo.send_message(instructions.instructions)
+
+with open("products.txt","r") as f:
+    products=f.read()
+    convo.send_message(f"Here are the links for products images:\n\n{products}")
+
+
 def send(answer,sender,phone_id):
     url = f"https://graph.facebook.com/v19.0/{phone_id}/messages"
     headers = {
         'Authorization': f'Bearer {wa_token}',
         'Content-Type': 'application/json'
     }
+    type="text"
+    body="body"
+    content=answer
+    urls=extractor.find_urls(answer)
+    if len(urls)>0:
+        mime_type,_=guess_type(urls[0].split("/")[-1])
+        type=mime_type.split("/")[0]
+        body="link"
+        content=urls[0]
+        answer=answer.replace(urls[0],"\n")
     data = {
         "messaging_product": "whatsapp",
-        "to": f"{sender}",
-        "type": "text",
-        "text": {"body": f"{answer}"},
+        "to": sender,
+        "type": type,
+        type: {
+            body:content,
+            **({"caption":answer} if type!="text" else {})
+        },
     }
     response = requests.post(url, headers=headers, json=data)
+    if db:
+        insert_chat("Bot",answer)
     return response
 
 def remove(*file_paths):
@@ -67,6 +90,118 @@ def remove(*file_paths):
         if os.path.exists(file):
             os.remove(file)
         else:pass
+
+if db:
+    db_url=os.environ.get("POSTGRES_URL") # Database URL
+    connect=psycopg2.connect(db_url)
+    cursor=connect.cursor()
+    scheduler = sched.scheduler(time.time, time.sleep)
+    report_time = datetime.now().replace(hour=22, minute=00, second=0, microsecond=0)
+
+    def insert_chat(sender,message):
+        cursor.execute("INSERT INTO chats (sender,message) VALUES (%s,%s)",(sender,message))
+        connect.commit()
+        cursor.close()
+
+    def get_chats(sender):
+        cursor.execute("SELECT chat_text FROM chats WHERE sender = %s", (sender,))
+        chats=cursor.fetchall()
+        cursor.close()
+        return chats
+    
+    def delete_old_chats():
+        cutoff_date = datetime.now() - timedelta(days=14)
+        cursor.execute("DELETE FROM chats WHERE timestamp < %s", (cutoff_date,))
+        connect.commit()
+        cursor.close()
+    
+    def create_report(phone_id):
+        today=datetime.today().strftime('%d-%m-%Y')
+        query = f"SELECT chat_content FROM chats WHERE date_trunc('day', chat_timestamp) = %s"
+        cursor.execute(query, (today,))
+        chats = cursor.fetchall()
+        if chats !="":
+            chats=today+"\n\n"+chats
+            send(chats,owner_phone,phone_id)
+        cursor.close()
+        connect.close()
+
+else:pass
+
+notification=lambda message,phone_id:send(message,owner_phone,phone_id)
+
+def message_handler(data,phone_id):
+    sender=data["from"]
+    if data["type"] == "text":
+        prompt = data["text"]["body"]
+        insert_chat(sender,prompt)
+        convo.send_message(prompt)
+    else:
+        media_url_endpoint = f'https://graph.facebook.com/v19.0/{data[data["type"]]["id"]}/'
+        headers = {'Authorization': f'Bearer {wa_token}'}
+        media_response = requests.get(media_url_endpoint, headers=headers)
+        media_url = media_response.json()["url"]
+        media_download_response = requests.get(media_url, headers=headers)
+        if data["type"] == "audio":
+            filename = "/tmp/temp_audio.mp3"
+        elif data["type"] == "image":
+            filename = "/tmp/temp_image.jpg"
+        elif data["type"] == "document":
+            doc=fitz.open(stream=media_download_response.content,filetype="pdf")
+            for _,page in enumerate(doc):
+                destination="/tmp/temp_image.jpg"
+                pix = page.get_pixmap()
+                pix.save(destination)
+                file = genai.upload_file(path=destination,display_name="tempfile")
+                response = model.generate_content(["Read this document carefully and explain it in detail",file])
+                answer=response._result.candidates[0].content.parts[0].text
+                convo.send_message(f'''Direct image input has limitations,
+                                    so this message is created by an llm model based on the document send by the user, 
+                                    reply to the customer assuming you saw that document.
+                                    (Warn the customer and stop the chat if it is not related to the business): {answer}''')
+                remove(destination)
+        else:send("This format is not Supported by the bot ☹",sender,phone_id)
+        if data["type"] == "image" or data["type"] == "audio":
+            with open(filename, "wb") as temp_media:
+                temp_media.write(media_download_response.content)
+            file = genai.upload_file(path=filename,display_name="tempfile")
+            if data["type"] == "image":
+                response = model.generate_content(["What is in this image?",file])
+                answer=response.text
+                convo.send_message(f'''Customer has sent an image,
+                                    So here is the llm's reply based on the image sent by the customer:{answer}\n\n''')
+                urls=extractor.find_urls(convo.last.text)
+                if len(urls)>0:
+                    headers_new={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+                    prod_response=requests.get(urls[0],headers=headers_new)
+                    img_path="/tmp/prod_image.jpg"
+                    with open(img_path, "wb") as temp_media:
+                        temp_media.write(prod_response.content)
+                    img = genai.upload_file(path=filename,display_name="tempfile")
+                    response=model.generate_content(["Is the things in both the images are exactly same? Explain in detail",img,file])
+                    answer=response.text
+                    convo.send_message(f'''This is the message from AI after comparing the two images: {answer}''')
+            else:
+                response = model.generate_content(["What is the content of this audio file?",file])
+                answer=response.text
+                convo.send_message(f'''Direct media input has limitations,
+                                            so this message is created by an llm model based on the audio send by the user, 
+                                            reply to the customer assuming you heard that audio.
+                                            (Warn the customer and stop the chat if it is not related to the business): {answer}''')
+            remove("/tmp/temp_image.jpg","/tmp/temp_audio.mp3","/tmp/prod_image.jpg")
+        files=genai.list_files()
+        for file in files:
+            file.delete()
+    reply=convo.last.text
+    if "unable_to_solve_query" in reply:
+        send(f"customer {sender} is not satisfied", owner_phone, phone_id)
+        reply=reply.replace("unable_to_solve_query",'\n')
+        send(reply, sender, phone_id)
+    else:send(reply,sender,phone_id)
+    if db:
+        scheduler.enterabs(report_time.timestamp(), 1, create_report, (phone_id,))
+        scheduler.run()
+        delete_old_chats()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -86,46 +221,9 @@ def webhook():
         try:
             data = request.get_json()["entry"][0]["changes"][0]["value"]["messages"][0]
             phone_id=request.get_json()["entry"][0]["changes"][0]["value"]["metadata"]["phone_number_id"]
-            sender="+"+data["from"]
-            if data["type"] == "text":
-                prompt = data["text"]["body"]
-                convo.send_message(prompt)
-                send(convo.last.text,sender,phone_id)
-            else:
-                media_url_endpoint = f'https://graph.facebook.com/v18.0/{data[data["type"]]["id"]}/'
-                headers = {'Authorization': f'Bearer {wa_token}'}
-                media_response = requests.get(media_url_endpoint, headers=headers)
-                media_url = media_response.json()["url"]
-                media_download_response = requests.get(media_url, headers=headers)
-                if data["type"] == "audio":
-                    filename = "/tmp/temp_audio.mp3"
-                elif data["type"] == "image":
-                    filename = "/tmp/temp_image.jpg"
-                elif data["type"] == "document":
-                    doc=fitz.open(stream=media_download_response.content,filetype="pdf")
-                    for _,page in enumerate(doc):
-                        destination="/tmp/temp_image.jpg"
-                        pix = page.get_pixmap()
-                        pix.save(destination)
-                        file = genai.upload_file(path=destination,display_name="tempfile")
-                        response = model.generate_content(["What is this",file])
-                        answer=response._result.candidates[0].content.parts[0].text
-                        convo.send_message(f"user(me) can't send media files directly to you. So this message is created by an llm model based on the image prompt from user to you, reply to the user based on this: {answer}")
-                        send(convo.last.text,sender,phone_id)
-                        remove(destination)
-                else:send("This format is not Supported by the bot ☹",sender,phone_id)
-                with open(filename, "wb") as temp_media:
-                    temp_media.write(media_download_response.content)
-                file = genai.upload_file(path=filename,display_name="tempfile")
-                response = model.generate_content(["What is this",file])
-                answer=response._result.candidates[0].content.parts[0].text
-                remove("/tmp/temp_image.jpg","/tmp/temp_audio.mp3")
-                convo.send_message(f"I can't send media files directly to you. So this is an voice/image message to you from me(user) transcribed by an llm model, reply to me assuming you saw the media file: {answer}")
-                send(convo.last.text,sender,phone_id)
-                files=genai.list_files()
-                for file in files:
-                    file.delete()
+            message_handler(data,phone_id)
         except :pass
         return jsonify({"status": "ok"}), 200
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    else:return "WhatsApp Bot is Running"
+
+if __name__ == "__main__":app.run(debug=True, port=8000)
